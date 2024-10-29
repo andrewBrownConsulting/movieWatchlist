697659e91f3ee127e1b8527eb3925cf3f08523e1
andrewBrownConsulting <andrewBrownConsult@gmail.com>
2024-10-29 19:02:17 +0000

    set up firebase

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,27 @@ import AddMovie from "@/components/addMovie";
 import DisplayMovies from "@/components/displayMovies";
 import { useState } from "react";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDGI5Z8KKCj-JnbBzyqsniGXQPMaUo3g5o",
+  authDomain: "moviewatchlist-3831d.firebaseapp.com",
+  projectId: "moviewatchlist-3831d",
+  storageBucket: "moviewatchlist-3831d.appspot.com",
+  messagingSenderId: "469596024562",
+  appId: "1:469596024562:web:d8e9d01537ae65f58baa6b",
+  measurementId: "G-N4MDRQRFNB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export default function Home() {
   const [movieList, setMovieList] = useState<Movie[] | null>();
   return (
